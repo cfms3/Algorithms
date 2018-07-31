046005a0d94cbe55e30a7a86c0bca006bf193788
cfms3 <FFebMorais@gmail.com>
2018-07-31 01:32:45 -0300

    Add files via upload

---
+++ HLD.cpp
@@ -0,0 +1,41 @@
+int pai[N], son[N], sz[N], h[N];
+
+void dfs_pre(int v, int par, int dep) {
+	pai[v] = par;
+	h[v] = dep;
+	sz[v] = 1;
+	son[v] = -1;
+	for(int u : adj[v]) {
+		if (u == par) continue;
+		dfs_pre(u, v, dep + 1);
+		sz[v] += sz[u];
+		//escolhe o mais pesado
+		if (son[v] == -1 || sz[son[v]] < sz[u]) {
+			son[v] = u;
+		}
+	}
+}
+
+int comp[N], pos[N], pos_in_comp[N], inv_pos[N], head[N], cur_comp = 0, cur_pos = 0;
+
+void build_hld(int v, int pos_comp) {
+	//seta as posiçoes
+	comp[v] = cur_comp;
+	pos[v] = cur_pos++;
+	pos_in_comp[v] = pos_comp;
+	inv_pos[pos[v]] = v;
+	
+	//primeiro da chain
+	if (pos_comp == 0) head[cur_comp] = v;
+	
+	if (son[v] != -1) { //nao é leaf
+		build_hld(son[v], pos_comp + 1);
+	} else {
+		//a chain terminou
+		cur_comp++;
+	}
+	for(int u : adj[v]) {
+		if (u == pai[v] || u == son[v]) continue;
+		build_hld(u, 0);
+	}
+}
---
+++ SegmentTree.cpp
@@ -0,0 +1,54 @@
+template<typename Info>
+class SegmentTree {
+private:
+	int n;
+	vector<Info> tree;
+	
+	Info merge(Info &l, Info &r) {
+		return min(l, r);
+	}
+	
+public:
+	SegmentTree() {}
+	SegmentTree(int k) {
+		n = k + 1;
+		tree.resize(n << 1);
+	}
+	SegmentTree(vector<Info> v) {
+		n = (int) v.size();
+		tree.resize(n << 1);
+		for(int i = 0; i < n; i++) {
+			tree[i + n] = v[i];
+		}
+		for(int i = n - 1; i >= 1; i--) {
+			tree[i] = merge(tree[i + i], tree[i + i + 1]);
+		}
+	}
+	
+	void init(int k) {
+		n = k + 1;
+		tree.resize(n << 1);
+	}
+	
+	void up(int pos, Info val) {
+		pos += n;
+		tree[pos] = val;
+		while(pos > 1) {
+			pos >>= 1;
+			tree[pos] = merge(tree[pos + pos], tree[pos + pos + 1]);
+		}
+	}
+	
+	Info get(int l, int r) {
+		Info ansl = 1e9, ansr = ansl;
+		for(l += n, r += n + 1; l < r; l >>= 1, r >>= 1) {
+			if (l & 1) {
+				ansl = merge(ansl, tree[l++]);
+			}
+			if (r & 1) {
+				ansr = merge(ansr, tree[--r]);
+			}
+		}
+		return merge(ansl, ansr);
+	}
+};
