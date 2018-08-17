d249cdb8a08015db86d632fe9f1bc58dad8d1194
Clodes Fernando <cfms3@cin.ufpe.br>
2018-08-16 23:20:49 -0300

    debug

---
+++ debug.cpp
@@ -0,0 +1,8 @@
+#define dbg(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+
+void err(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) {
+	cerr << *it << " = " << a << endl;
+	err(++it, args...);
+}
