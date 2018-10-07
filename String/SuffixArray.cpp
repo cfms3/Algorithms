30d57a76fdd63ba9b2bc09145a356e475ba94df9
Clodes Fernando <cfms3@cin.ufpe.br>
2018-10-06 23:02:52 -0300

    Suffix array O(nlogn) add

--- String/SuffixArray.cpp
+++ String/SuffixArray.cpp
@@ -59,3 +59,81 @@ namespace SA {
 		return lcp;
 	}
 };
+
+//nlogn
+int sa[N], pos[N], lcp[N], number[N], first[N], second[N], temp[N];
+
+int get_id(char c) {
+    return c;
+}
+
+void buildSA(string &s, int n) {
+    for (int i = 0; i < n; i++) {
+        number[i] = 0;
+    }
+    for (int i = 0; i < n; i++) {
+        ++number[get_id(s[i])];
+    }
+    for (int i = 1; i < sigma; i++) {
+        number[i] += number[i - 1];
+    }
+    for (int i = 0; i < n; i++) {
+        sa[--number[get_id(s[i])]] = i;
+    }
+    for (int i = 0; i <= n; i++) {
+        number[i] = 0;
+    }
+    pos[sa[0]] = 0;
+    for (int i = 1; i < n; i++) {
+        pos[sa[i]] = pos[sa[i - 1]];
+        if (s[sa[i - 1]] != s[sa[i]]) pos[sa[i]]++;
+    }
+    for (int offset = 1; offset < n; offset <<= 1) {
+        for (int i = 0; i < n; i++) {
+            first[i] = pos[i];
+            second[i] = i + offset >= n ? 0 : pos[i + offset] + 1;
+        }
+        for (int i = 0; i < n; i++) {
+            ++number[second[i]];
+        }
+        for (int i = 1; i <= n; i++) {
+            number[i] += number[i - 1];
+        }
+        for (int i = 0; i < n; i++) {
+            temp[--number[second[i]]] = i;
+        }
+        for (int i = 0; i <= n; i++) {
+            number[i] = 0;
+        }
+        for (int i = 0; i < n; i++) {
+            ++number[first[i]];        
+        }
+        for (int i = 1; i <= n; i++) {
+            number[i] += number[i - 1];
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            sa[--number[first[temp[i]]]] = temp[i];
+        }
+        for (int i = 0; i <= n; i++) {
+            number[i] = 0;
+        }
+        pos[sa[0]] = 0;
+        for (int i = 1; i < n; i++) {
+            pos[sa[i]] = pos[sa[i - 1]] + (first[sa[i]] != first[sa[i - 1]] || second[sa[i]] != second[sa[i - 1]]);
+        }
+    }
+}
+
+void buildLCP(string &s, int n) {
+    int k = 0;
+    for(int i = 0; i < n; i++) {
+        if (pos[i] + 1 == n) {
+            k = 0;
+            continue;
+        }	
+        int j = sa[pos[i] + 1];
+        while(i + k < n && j + k < n && s[i + k] == s[j + k]) k++;
+        lcp[pos[i]] = k;
+        k = max(k - 1, 0);
+    }
+}
